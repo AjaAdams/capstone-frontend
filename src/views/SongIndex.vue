00c3515ab21089bd95ababf8bc2bd1f5ced251e3
AjaAdams <ajajhendrix@gmail.com>
2020-12-11 14:40:18 -0600

    initial commit

---
+++ src/views/SongIndex.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="songs">
+    <h1>All Songs</h1>
+    <div v-for="song in songs">
+      <img v-bind:src="song.image_url" v-bind:alt="song.album">
+      <h2>{{ song.title }}</h2>
+      <h3>{{ song.artist }}</h3>
+      <h3>{{ song.album }}</h3>
+      <h3>{{ song.year }}</h3>
+      <h4>{{ song.genre }}</h4>
+      
+
+    </div>
+  </div>
+</template>
+
+<style>
+  img {
+    width: 150px;
+  }
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      songs: []
+    };
+  },
+  created: function() {
+    axios
+      .get("/api/songs")
+      .then(response => {
+        console.log("songs index", response);
+        this.songs = response.data;
+      });
+  },
+  methods: {}
+};
+</script>
---
+++ src/views/SubjectIndex.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="subjects">
+    <h1>All Subjects</h1>
+    <div v-for="subject in subjects">
+      <h2>{{ subject.name }}</h2>
+
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      subjects: []
+    };
+  },
+  created: function() {
+    axios
+      .get("/api/subjects")
+      .then(response => {
+        console.log("subjects index", response);
+        this.subjects = response.data;
+      });
+  },
+  methods: {}
+};
+</script>
